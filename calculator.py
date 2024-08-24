0d4168bcdf42ef0c2a748e83831dcf1e37b362b2
shivamsonari376 <shivamsonari376@gmail.com>
2024-08-24 15:40:39 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -8,6 +8,8 @@ class Calculator:
     def multiply(self, a, b):
         return a * b
     def divide(self, a, b):
+        if b == 0:
+            raise ValueError("Cannot divide by zero.")
         return a / b
 
 if __name__ == "__main__":
