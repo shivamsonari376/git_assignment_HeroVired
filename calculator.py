dce3ffee4e7af5d4244e1973f4f2fc13cf525a1d
shivamsonari376 <shivamsonari376@gmail.com>
2024-08-24 14:35:45 +0530

    Update calculator.py with feature of square root

--- calculator.py
+++ calculator.py
@@ -9,6 +9,8 @@ class Calculator:
         return a * b
     def divide(self, a, b):
         return a / b
+    def square_root(self, x):
+        return math.sqrt(x)
 
 if __name__ == "__main__":
     
@@ -19,3 +21,5 @@ if __name__ == "__main__":
     print(f"{num1} - {num2} = {calculator.subtract(num1, num2)}") 
     print(f"{num1} * {num2} = {calculator.multiply(num1, num2)}")
     print(f"{num1} / {num2} = {calculator.divide(num1, num2)}")
+    num3 = 25
+    print(f"The square root of {num3} = {calculator.square_root(num3)}")
